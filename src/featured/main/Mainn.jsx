1e6db724551ce8d688aeea5331f07099a808e28f
Ulvi <ulvijalil7@gmail.com>
2024-08-11 01:14:47 +0400

    feat: Main folder created

--- src/featured/main/Mainn.jsx
+++ src/featured/main/Mainn.jsx
@@ -7,9 +7,19 @@ const Main = () => {
 <div className='wrapper-container'>
 
 <div className="wrapper-girl">
+  <div className="wrapper-girl-content">
+    <button>GADGETS</button>
+    <h1>Save $25 on Philips Wired Headphone For A </h1>
+    <h1 className='secondh1'>Great Sounding Over-Ear Headphone</h1>
+  </div>
+</div>
+{/* End of Wrapper Girl */}
+
+<div className="wrapper-cards-container">
+<div className="wcard">
   
 </div>
-
+</div>
 
 </div>
   )
