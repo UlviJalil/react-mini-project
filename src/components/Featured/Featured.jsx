246f701d96dd5fd7b6c77216c16405a21e4ba6ed
Ulvi <ulvijalil7@gmail.com>
2024-08-14 15:54:54 +0400

    feat: Created footer

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -4,89 +4,89 @@ import './featured.scss'
 const Featured = () => {
   return (
     <>
-    <h1 className='feat'>Featured Reviews</h1>
-    <div className='featured-container'>
-    
-      <div className='featured-text'>
+      <h1 className='feat'>Featured Reviews</h1>
+      <div className='featured-container'>
 
-        <div className="feat-content">
-          <button  className='gbtn'>GAMING</button>
-          <h1>Pico 4 Review: Should You Actually <br />
-            Buy One Instead Of Quest 2?</h1>
-          <p>To understand the new smart watched and other pro devices of recent focus, <br /> we should look to…</p>
+        <div className='featured-text'>
+
+          <div className="feat-content">
+            <button className='gbtn'>GAMING</button>
+            <h1>Pico 4 Review: Should You Actually <br />
+              Buy One Instead Of Quest 2?</h1>
+            <p>To understand the new smart watched and other pro devices of recent focus, <br /> we should look to…</p>
+          </div>
+
         </div>
 
+        <div className="featured-img">
+
+        </div>
       </div>
 
-      <div className="featured-img">
+      <div className='card-container'>
 
-      </div>
-    </div>
 
-    <div className='card-container'>
-      
-
-      <div className="card-wrapper">
-        <div className="card-img">
-          <button>GADGETS</button>
-          <div className="card-text-wrapper">
-          <div className="card-content">
-            <h2>Study: Earbuds Use, <br /> Youngsters at High Risk of <br /> Hearing Loss</h2>
-            <div className="alt-text">
-              <p>Shane Doe</p>
-              <p className='secondp'>Mar 15, 2020</p>
+        <div className="card-wrapper">
+          <div className="card-img">
+            <button>GADGETS</button>
+            <div className="card-text-wrapper">
+              <div className="card-content">
+                <h2>Study: Earbuds Use, <br /> Youngsters at High Risk of <br /> Hearing Loss</h2>
+                <div className="alt-text">
+                  <p>Shane Doe</p>
+                  <p className='secondp'>Mar 15, 2020</p>
+                </div>
               </div>
-          </div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="card-wrapper2">
-        <div className="card-img2">
-          <button>TECHNOLOGY</button>
-          <div className="card-text-wrapper2">
-          <div className="card-content2">
-            <h2>Telescope is Revealing the <br /> Galaxies of the Universe Like <br /> Never Before</h2>
-            <div className="alt-text2">
-              <p>Shane Doe</p>
-              <p className='secondp2'>Mar 15, 2020</p>
+        <div className="card-wrapper2">
+          <div className="card-img2">
+            <button>TECHNOLOGY</button>
+            <div className="card-text-wrapper2">
+              <div className="card-content2">
+                <h2>Telescope is Revealing the <br /> Galaxies of the Universe Like <br /> Never Before</h2>
+                <div className="alt-text2">
+                  <p>Shane Doe</p>
+                  <p className='secondp2'>Mar 15, 2020</p>
+                </div>
               </div>
-          </div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="card-wrapper3">
-        <div className="card-img3">
-          <button>PHONES</button>
-          <div className="card-text-wrapper3">
-          <div className="card-content3">
-            <h2>CarPlay Concept Shows Off a <br />Modular UI Inspired by Next- <br />Gen Design</h2>
-            <div className="alt-text3">
-              <p>Shane Doe</p>
-              <p className='secondp3'>Mar 15, 2020</p>
+        <div className="card-wrapper3">
+          <div className="card-img3">
+            <button>PHONES</button>
+            <div className="card-text-wrapper3">
+              <div className="card-content3">
+                <h2>CarPlay Concept Shows Off a <br />Modular UI Inspired by Next- <br />Gen Design</h2>
+                <div className="alt-text3">
+                  <p>Shane Doe</p>
+                  <p className='secondp3'>Mar 15, 2020</p>
+                </div>
               </div>
-          </div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="card-wrapper4">
-        <div className="card-img4">
-          <button>GADGETS</button>
-          <div className="card-text-wrapper4">
-          <div className="card-content4">
-            <h2>Latest Windows 11 Preview <br /> Build Finally Lets You Search <br /> for Copied Text</h2>
-            <div className="alt-text4">
-              <p>Shane Doe</p>
-              <p className='secondp4'>Mar 15, 2020</p>
+        <div className="card-wrapper4">
+          <div className="card-img4">
+            <button>GADGETS</button>
+            <div className="card-text-wrapper4">
+              <div className="card-content4">
+                <h2>Latest Windows 11 Preview <br /> Build Finally Lets You Search <br /> for Copied Text</h2>
+                <div className="alt-text4">
+                  <p>Shane Doe</p>
+                  <p className='secondp4'>Mar 15, 2020</p>
+                </div>
               </div>
-          </div>
+            </div>
           </div>
         </div>
-      </div>
 
-    </div>
+      </div>
 
     </>
   )
--- src/featured/footer/Footerr.jsx
+++ src/featured/footer/Footerr.jsx
@@ -1,15 +1,181 @@
 import React from 'react'
-// import { FaFacebookF, FaInstagram, FaPinterestP } from "react-icons/fa";
-// import { FaXTwitter } from "react-icons/fa6";
-// import { ImFire } from "react-icons/im";
+import { FaFacebookF, FaInstagram, FaPinterestP } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { ImFire } from "react-icons/im";
 
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="p-0 bg-[#191919] w-full">
+      <div className="w-full flex bg-[#161616] py-10">
+        <div className="flex pr-8 gap-10">
+          <div className="flex flex-col gap-4 w-1/2 pl-[105px] pr-2.5 ">
+            <h5 className="text-white mb-4 text-lg">Most Popular</h5>
+            <div className="flex flex-col gap-4  divide-y divide-gray-600 pr-2.5 ">
+              <div className="flex items-center gap-4 w-[380px] h-[75px] pr-2.5 ">
+                <img
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2021/01/kamil-s-SuPAbuuK7f4-unsplash-150x100.jpg"
+                  alt="Popular Xbox Game"
+                  className="w-[97px] h-[74px] object-cover rounded-[10px]"
+                />
+                <div>
+                  <p className="text-white text-sm hover:text-[#3C3FDE] cursor-pointer	 ">
+                    Popular New Xbox Game Pass Game Being Review Bombed With “Os”
+                  </p>
+                  <div className="flex items-center text-gray-400 text-xs ">
+                    <p>Jan 14, 2021</p>
+                    <span className="flex items-center text-red-600 ml-2">
+                      <ImFire className="mr-1" /> 7,792 Views
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4 w-[380px] h-[75px] pt-5">
+                <img
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2021/01/jonathan-kemper-poSms2EzfNY-unsplash-150x100.jpg"
+                  alt="Samsung MicroLED"
+                  className="w-[97px] h-[74px] object-cover rounded-[10px]"
+                />
+                <div>
+                  <p className="text-white text-sm hover:text-[#3C3FDE] cursor-pointer	">
+                    Samsung is Developing Bright MicroLED on Displays for AR Headsets
+                  </p>
+                  <div className="flex items-center text-gray-400 text-xs">
+                    <p>Jan 14, 2021</p>
+                    <span className="flex items-center text-red-600 ml-2">
+                      <ImFire className="mr-1" /> 6,914 Views
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4 w-[380px] h-[75px] pt-5">
+                <img
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2021/01/0247d91565b69ed9ddba4ec59ac97963-1-150x86.jpg"
+                  alt="Mortal Kombat Announcement"
+                  className="w-[97px] h-[74px] object-cover rounded-[10px]"
+                />
+                <div>
+                  <p className="text-white text-sm hover:text-[#3C3FDE] cursor-pointer	">
+                    Warner Bros. Games Announces Mortal Kombat: Onslaught
+                  </p>
+                  <div className="flex items-center text-gray-400 text-xs">
+                    <p>Jan 14, 2021</p>
+                    <span className="flex items-center text-red-600 ml-2">
+                      <ImFire className="mr-1" /> 4,418 Views
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 w-1/2">
+            <h5 className="text-white mb-4 text-lg">Our Picks</h5>
+            <div className="flex flex-col gap-4 divide-y divide-gray-600">
+              <div className="flex items-center gap-4">
+                <img
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/thisisengineering-raeng-a7A9O5htrKs-unsplash-150x100.jpg"
+                  alt="Game Development"
+                  className="w-[97px] h-[74px] object-cover rounded-[10px]"
+                />
+                <div>
+                  <p className="text-white text-sm hover:text-[#3C3FDE] cursor-pointer	">
+                    Game Development This Week: Save On Essential Tools and More
+                  </p>
+                  <div className="flex items-center text-gray-400 text-xs">
+                    <p>Nov 19, 2022</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4 w-[380px] h-[75px] pt-5">
+                <img
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/Depositphotos_242235046_xl-2015-1-150x85.jpg"
+                  alt="Mortal Kombat Announcement"
+                  className="w-[97px] h-[74px] object-cover rounded-[10px]"
+                />
+                <div>
+                  <p className="text-white text-sm hover:text-[#3C3FDE] cursor-pointer	">
+                    Is the Hyperloop Doomed? What Elon Musk’s Latest Setback Really Means
+                  </p>
+                  <div className="flex items-center text-gray-400 text-xs">
+                    <p>Jan 14, 2021</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4 w-[380px] h-[75px] pt-5">
+                <img
+                  src="https://smartmag.theme-sphere.com/tech-blog/wp-content/uploads/sites/35/2022/11/remy_loz-3vVzdb8KKIw-unsplash-150x100.jpg"
+                  alt="Mortal Kombat Announcement"
+                  className="w-[97px] h-[74px] object-cover rounded-[10px]"
+                />
+                <div>
+                  <p className="text-white text-sm hover:text-[#3C3FDE] cursor-pointer	">
+                    The Best Early Black Friday Deals on Gaming Laptops and Accessories
+                  </p>
+                  <div className="flex items-center text-gray-400 text-xs">
+                    <p>Jan 14, 2021</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-[369px] h-[349px] bg-[#000] flex flex-col items-center mr-10">
+          <h3 className="text-[#ECECEC] text-[24px] pt-[26px]">Subscribe to Updates</h3>
+          <p className="text-[#CCC] text-center w-[296px] h-[48px] pt-[10px]">
+            Get the latest creative news from FooBar about art, design and business.
+          </p>
+          <div className="flex flex-col items-center space-y-4 pt-9">
+            <input
+              className="w-[296px] h-[44px] px-4 rounded-[10px] border border-[#CCC] bg-[#151515] text-[#CCC] text-center focus:outline-none focus:border-[#3C3FDE]"
+              type="email"
+              name="EMAIL"
+              placeholder="Your email address..."
+            />
+            <input
+              className="w-[296px] h-[44px] bg-[#3C3FDE] text-white rounded-[10px] cursor-pointer hover:bg-[#151515] focus:outline-none"
+              type="submit"
+              value="Subscribe"
+            />
+            <label className="text-[#CCC] text-xs flex items-center gap-2 text-left">
+              <input type="checkbox" className="mt-0" />
+              <p className="m-0">By signing up, you agree to our terms and Privacy Policy.</p>
+            </label>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="w-full h-[266px] bg-[#070707] flex flex-col justify-center items-center gap-4">
+        <div className="text-white flex gap-6 text-lg">
+          <div className="p-3 bg-[#333] rounded-full cursor-pointer hover:text-[#2e32f1]">
+            <FaFacebookF />
+          </div>
+          <div className="p-3 bg-[#333] rounded-full cursor-pointer hover:text-[#2e32f1]">
+            <FaXTwitter />
+          </div>
+          <div className="p-3 bg-[#333] rounded-full cursor-pointer hover:text-[#2e32f1]">
+            <FaInstagram />
+          </div>
+          <div className="p-3 bg-[#333] rounded-full cursor-pointer hover:text-[#2e32f1]">
+            <FaPinterestP />
+          </div>
+        </div>
+        <div className="text-white text-sm flex gap-5">
+          <p className="cursor-pointer hover:text-gray-400">HOME</p>
+          <p className="cursor-pointer hover:text-gray-400">PHONES</p>
+          <p className="cursor-pointer hover:text-gray-400">ABOUT</p>
+          <p className="cursor-pointer hover:text-gray-400">CONTACT</p>
+        </div>
+        <p className="text-white text-xs mt-4">© 2024 ThemeSphere. Designed by ThemeSphere.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
